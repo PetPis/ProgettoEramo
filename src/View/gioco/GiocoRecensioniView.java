7384a71ebf64fda196025b418482d16aebf3e985
davidepetrini <davidepetrini96@gmail.com>
2018-03-27 12:07:44 +0200

    aggiunta view gioco, view moderatore, login

---
+++ src/View/gioco/GiocoRecensioniView.java
@@ -0,0 +1,56 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+
+public class GiocoRecensioniView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GiocoRecensioniView frame = new GiocoRecensioniView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GiocoRecensioniView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Recensioni del gioco:");
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblNewLabel.setBounds(41, 25, 193, 28);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(238, 24, 156, 28);
+		contentPane.add(lblNewLabel_1);
+	}
+
+}
